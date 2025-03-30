e72fa7d9b22cae5e9463df20abc2c93bd1bdbb0e
JanviChauhan045 <janvi.chauhan4599@gmail.com>
2025-03-30 10:20:55 +0530

    Removed all files

--- backend/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import user_status
-
-urlpatterns = [
-    path('user-status/', user_status),
-]
--- backend/api/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import JsonResponse
-
-def user_status(request):
-    return JsonResponse({"message": "User Connected"})
